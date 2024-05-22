ca1ff205af398d54d562627229b1e701c36dec31
Sathya4162 <95sath@gmail.com>
2024-05-22 12:19:21 +0530

    i completed my work

---
+++ src/org/ScannerModel/Marks.java
@@ -0,0 +1,14 @@
+package org.ScannerModel;
+
+import java.util.Scanner;
+
+public class Marks {
+
+ public static void main(String[] args) {
+	
+	 Scanner s = new Scanner(System.in);
+
+	 
+	 
+}
+}
