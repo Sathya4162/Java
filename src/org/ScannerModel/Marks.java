b0392c1b6044e9b17057a9da9c459b5c16fa3733
Sathya4162 <95sath@gmail.com>
2024-05-22 22:54:52 +0530

    Bala task updated

--- src/org/ScannerModel/Marks.java
+++ src/org/ScannerModel/Marks.java
@@ -8,7 +8,13 @@ public class Marks {
 	
 	 Scanner s = new Scanner(System.in);
 
+	 System.out.println("Enter a name");
+	 String next = s.next();
+	 System.out.println("name: " + next);
 	 
-	 
+	 System.out.println("Enter your class");
+	 String next1 = s.next();
+	 System.out.println("class: " + next1);
+	  
 }
 }
